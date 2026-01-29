5d96671f97f88c07513bdc715895565ebe17b701
Jem Galicia <jemgalicia.git@gmail.com>
2026-01-29 14:48:06 +0800

    changes

--- webpack.common.js
+++ webpack.common.js
@@ -6,7 +6,7 @@ module.exports = {
   output: {
     filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
-    // publicPath: '/orchard/',
+    publicPath: '/orchard/',
     clean: true
   },
   plugins: [
